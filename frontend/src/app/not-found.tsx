a1925adac54f763dcd81805f3f2829670832e7fa
Mostafa Hassan <m0st4f4h4ss4n@gmail.com>
2025-06-05 02:27:16 +0300

    feat(frontend): add 404 page

---
+++ frontend/src/app/not-found.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { Button, Flex, Heading, Text } from "@chakra-ui/react";
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <Flex direction="column" align="center" justify="center" minH="100vh" gap={6}>
+      <Heading as="h1" size="lg">
+        404 - Page Not Found
+      </Heading>
+      <Text>Sorry, the page you are looking for does not exist.</Text>
+      <Button as={Link} href="/" colorScheme="blue">
+        Go Home
+      </Button>
+    </Flex>
+  );
+}
