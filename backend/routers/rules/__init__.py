543fe454b5849b6da2a96228448ffac0eebd6094
Mostafa Hassan <m0st4f4h4ss4n@gmail.com>
2025-06-05 14:32:28 +0300

    feat: add handoff criteria router (#295)

--- backend/routers/rules/__init__.py
+++ backend/routers/rules/__init__.py
@@ -3,7 +3,7 @@ from .workflows.workflows import router as workflows_router
 from .violations.violations import router as violations_router
 from .violations.error_protocols import router as error_protocols_router
 from .templates.templates import router as templates_router
-from .roles.roles import router as roles_router
+from .roles import router as roles_router
 from .mandates.mandates import router as mandates_router
 from .logs.logs import router as logs_router
 
