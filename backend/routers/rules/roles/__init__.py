e2c18beeeeef28283ecbad3422c4f136c5b5d96b
Mostafa Hassan <m0st4f4h4ss4n@gmail.com>
2025-06-05 16:16:43 +0300

    feat: add verification requirement support

--- backend/routers/rules/roles/__init__.py
+++ backend/routers/rules/roles/__init__.py
@@ -3,9 +3,11 @@ from .roles import router as roles_router
 from .capabilities import router as capabilities_router
 from .forbidden_actions import router as forbidden_actions_router
 from .handoff_criteria import router as handoff_criteria_router
+from .verification_requirements import router as verification_requirements_router
 
 router = APIRouter()
 router.include_router(roles_router)
 router.include_router(capabilities_router)
 router.include_router(forbidden_actions_router)
 router.include_router(handoff_criteria_router)
+router.include_router(verification_requirements_router)
