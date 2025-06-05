8cb6a18b5ed94b2fc9ea1a0327bbabe5694016ef
Mostafa Hassan <m0st4f4h4ss4n@gmail.com>
2025-06-05 20:21:06 +0300

    feat: add custom error pages

--- frontend/src/app/not-found.tsx
+++ frontend/src/app/not-found.tsx
@@ -7,10 +7,8 @@ import Link from "next/link";
 export default function NotFound() {
   return (
     <Flex direction="column" align="center" justify="center" minH="100vh" gap={6}>
-      <Heading as="h1" size="lg">
-        404 - Page Not Found
-      </Heading>
-      <Text>Sorry, the page you are looking for does not exist.</Text>
+      <Heading as="h1" size="lg">Oops! Page Not Found</Heading>
+      <Text>The page you requested could not be found.</Text>
       <Button as={Link} href="/" colorScheme="blue">
         Go Home
       </Button>
